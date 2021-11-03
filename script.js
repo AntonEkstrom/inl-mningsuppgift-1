46c5e6699bc33943d61d3ea2df4f7c78af77b3fd
anton <ziick1234@live.se>
2021-11-03 19:57:12 +0100

    inlämingsuppgift 1

--- script.js
+++ script.js
@@ -10,9 +10,8 @@ const day = document.getElementById("day");
 const address = document.getElementById("address")
 const zipCode = document.getElementById("zipCode")
 const ort = document.getElementById("ort")
-const submit = document.getElementById("submit");
-const SubmitError = document.getElementById("sendBtn-error")
-const errorElement = document.getElementById('error')
+
+
 
 
 
@@ -27,12 +26,85 @@ const errorElement = document.getElementById('error')
     const lastNameValue = lastName.value.trim();
     const eMailValue = eMail.value.trim();
     const passwordValue = password.value.trim(); 
-    const conValue = .value.trim(); 
-    const Value = .value.trim(); 
-    const Value = .value.trim(); 
-    const Value = .value.trim(); 
-    const Value = .value.trim(); 
-    const Value = .value.trim(); 
-    const Value = .value.trim(); 
+    const confirmPasswordValue = confirmPassword.value.trim(); 
+    const birthDateValue = birthDate.value.trim();
+    const addressValue = address.value.trim(); 
+    
 
- }
+    if(firstNameValue < 2) {
+		setErrorFor(firstName, 'Förnamn måste vara minst 2 tecken');
+	} else {
+		setSuccessFor(firstName);
+	}
+	
+    if(lastNameValue < 2 ) {
+		setErrorFor(lastName, 'Efternamn måste vara minst 2 tecken');
+	} else {
+		setSuccessFor(lastName);
+	}
+
+	if(eMailValue === '') {
+		setErrorFor(eMail, 'Får ej vara tomt');
+	} else if (!isEmail(eMailValue)) {
+		setErrorFor(eMail, 'Inte en giltig e-postadress');
+	} else {
+		setSuccessFor(eMail);
+	}
+
+    if(birthDateValue === '') {
+		setErrorFor(birthDate, 'För att registrera sig så måste man vara över 18år.');
+	} else {
+		setSuccessFor(birthDate);
+	}
+
+    if(addressValue === '') {
+		setErrorFor(address, 'adress måste minst ha två tecken och postnumret måste vara fem tecken.');
+	} else {
+		setSuccessFor(address);
+	}
+
+
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Måste innehålla minst åtta tecken');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(confirmPasswordValue === '') {
+		setErrorFor(confirmPassword, 'Måste innehålla minst åtta tecken');
+	} else if(passwordValue !== confirmPasswordValue) {
+		setErrorFor(confirmPassword, 'Matchar inte med lösenord');
+	} else{
+		setSuccessFor(confirmPassword);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(eMail) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(eMail);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
